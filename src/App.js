c5d2abdb2e018ba7384849f15b0df160ac45d616
presstwice <38543504+presstwice@users.noreply.github.com>
2024-01-11 16:51:49 +1000

    x

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Timer from "./Timer";
+import Timer from './Timer';
+
 
 function App() {
   return (
     <main>
       <Timer />
-      test
     </main>
   );
 }
 
 export default App;
+ 
