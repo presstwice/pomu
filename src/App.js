e3063ffc3e4e0b440bdc9ac74b64d7b501b32073
presstwice <38543504+presstwice@users.noreply.github.com>
2024-01-18 11:32:20 +1000

    x

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Timer from "./Timer";
+import SettingsButton from "./SettingsButton";
+import {useState} from "react";
 
 
 function App() {
   return (
    <main>
+    <SettingsButton />
     <Timer />
     </main> 
   );
