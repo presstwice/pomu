3350ee1c52bf7bef33851757a14fe447a3ad0f57
presstwice <38543504+presstwice@users.noreply.github.com>
2024-01-20 22:40:23 +1000

    x

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import SettingsContext from './SettingsContext';
 function App() {
 
   const [showSettings, setShowSettings] = useState(false); 
-  const [workMinutes, setWorkMinutes] = useState(45);
-  const [breakMinutes, setBreakMinutes] = useState(15);
+  const [workMinutes, setWorkMinutes] = useState(25);
+  const [breakMinutes, setBreakMinutes] = useState(5);
 
   return (
    <main>
